c231ec30755d80d335595f77edb2c689b51c09de
joshartienda <143154592+joshartienda@users.noreply.github.com>
2024-12-02 18:47:16 -0800

    Add files via upload

---
+++ Proj 4/BackgroundScoller.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScoller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [Range(-1f, 1f)]
+    public float scrollSpeed = 0.5f;
+    public float offset;
+    private Material mat;
+    void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offset += (Time.deltaTime * scrollSpeed) / 2f;
+        mat.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+    }
+}
